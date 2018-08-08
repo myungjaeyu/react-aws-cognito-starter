4fd8ffae23cbce103c2a97a02631b5aff3f362f2
myungjaeyu <rootcode10@gmail.com>
2018-08-09 00:43:44 +0900

    Updated epic/auth.js
    - store stream.

--- src/redux/epics/auth.js
+++ src/redux/epics/auth.js
@@ -2,22 +2,40 @@ import { ActionTypes } from '../constants';
 import { loginSuccess, registSuccess, registConfirmSuccess } from '../actions';
 
 import { ofType } from 'redux-observable';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
-export const Login = (action$) => 
+export const Login = (action$, store$) => 
     action$.pipe(
         ofType(ActionTypes.AUTH_LOGIN),
+        tap(_ => {
+            let { login_email, login_password } = store$.value.form;
+
+            console.log('login epic:', login_email, login_password)
+
+        }),
         map(_ => loginSuccess({ data : ''}))
     );
 
-export const Regist = (action$) => 
+export const Regist = (action$, store$) => 
     action$.pipe(
         ofType(ActionTypes.AUTH_REGIST),
+        tap(_ => {
+            let { regist_email, regist_password } = store$.value.form;
+
+            console.log('regist epic:', regist_email, regist_password)
+
+        }),        
         map(_ => registSuccess({ data : ''}))
     );
 
-export const RegistConfirm = (action$) => 
+export const RegistConfirm = (action$, store$) => 
     action$.pipe(
         ofType(ActionTypes.AUTH_REGIST_CONFIRM),
+        tap(_ => {
+            let { regist_confirm_email, regist_confirm_code } = store$.value.form;
+
+            console.log('regist_confirm epic:', regist_confirm_email, regist_confirm_code)
+
+        }),
         map(_ => registConfirmSuccess({ data : ''}))
     );
