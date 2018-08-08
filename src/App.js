df6d50979b6d4d88fb3f06c171efdd5a480b34cb
myungjaeyu <rootcode10@gmail.com>
2018-08-08 21:17:44 +0900

    Added Login Form.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,58 @@ import './App.css';
 
 import { connect } from 'react-redux';
 
+
+
+const Login = ({ handleChange, handleSubmit }) => (
+    <form  name='login' onSubmit={ handleSubmit }>
+        <label>
+            Email
+            <input type="text" name='email' onChange={ handleChange } />
+        </label>
+
+        <label>
+            Password
+            <input type="password" name='password' onChange={ handleChange } />
+        </label>
+
+        <input type="submit" value="Submit" />
+    </form>
+);
+
+
+
 class App extends Component {
     render() {
 
-        const { fetch } = this.props;
+        const { fetch, handleChange, handleSubmit } = this.props;
+
         console.log(fetch);
 
         return (
         <div className="App">
-
+            <Login handleChange={ handleChange } handleSubmit={ handleSubmit } />
         </div>
         );
     }
 }
+
+
 
 const mapState = (state, ownProps) => ({
     fetch : state.fetch
 });
 
 const mapDispatch = (dispatch, ownProps) => ({
+    handleChange : e => {
+        let { name, value } = e.target;
 
+        console.log(name, value);
+    },
+    handleSubmit : e => {
+        e.preventDefault();
+
+        console.log(e.target.name);
+    }
 });
 
 export default connect(mapState, mapDispatch)(App);
