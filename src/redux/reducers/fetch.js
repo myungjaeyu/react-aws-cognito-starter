9fc3173094d03ef68e34eeeab6cc306b3709bc60
myungjaeyu <rootcode10@gmail.com>
2018-08-08 19:36:17 +0900

    Added reducers.

---
+++ src/redux/reducers/fetch.js
@@ -0,0 +1,31 @@
+import immutable from 'immutability-helper';
+import createReducer from '../createReducer';
+
+import { ActionTypes } from '../constants';
+
+const fetchState = {
+    pending: false,
+    data : {}
+};
+
+export default {
+    mockData : createReducer(fetchState, {
+        [ActionTypes.FETCH](state) {
+            return immutable(state, {
+                pending: { 
+                    $set : true
+                }
+            });
+        },
+        [ActionTypes.FETCH_SUCCESS](state, { data }) {
+            return immutable(state, {
+                pending: { 
+                    $set : false
+                },
+                data : {
+                    $set : data
+                }
+            });
+        },
+    })
+};
