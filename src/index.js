ad05bcae093527d3cbf56f35232a770da4a86b40
myungjaeyu <rootcode10@gmail.com>
2018-08-09 03:58:41 +0900

    Deleted css and unit test file.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 import { configureStore } from './redux/store';
 
@@ -13,4 +11,3 @@ ReactDOM.render(
     <Provider store={ store }>
         <App />
     </Provider>, document.getElementById('root'));
-registerServiceWorker();
