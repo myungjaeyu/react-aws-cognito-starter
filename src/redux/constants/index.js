4a36c225ad3364e07afbc9372398dea3a633e66a
myungjaeyu <rootcode10@gmail.com>
2018-08-08 22:25:40 +0900

    Added constants for auth.

--- src/redux/constants/index.js
+++ src/redux/constants/index.js
@@ -1,4 +1,10 @@
 export const ActionTypes = {
     FETCH : '[fetch] fetch',
-    FETCH_SUCCESS : '[fetch] fetchSuccess'
+    FETCH_SUCCESS : '[fetch] fetchSuccess',
+    AUTH_LOGIN : '[auth] login',
+    AUTH_LOGIN_SUCCESS : '[auth] loginSuccess',
+    AUTH_REGIST : '[auth] regist',
+    AUTH_REGIST_SUCCESS : '[auth] registSuccess',
+    AUTH_REGIST_CONFIRM : '[auth] registConfirm',
+    AUTH_REGIST_CONFIRM_SUCCESS : '[auth] registConfirmSuccess'
 }
